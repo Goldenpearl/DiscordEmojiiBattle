0fd88438497436af01a9894d1029b6f4ca0f70f1
Goldenpearl <kblair5@yahoo.com>
2019-06-24 22:42:30 -0400

    Adding boss infrastructure

--- src/boss.ts
+++ src/boss.ts
@@ -2,4 +2,37 @@ export class Boss{
   private emojiiCombo = "";
   private emojiiCombo2 = "";
   private emojiiCombo3 = "";
+
+  private outputHandler : OutputHandler;
+  constructor(outputHandler : OutputHandler)
+  {
+    this.outputHandler = outputHandler;
+  }
+  
+  /**
+  * Handles an emojii input during a boss fight
+  * @param {string} textInput the string to process. (Should be an emojii)
+  */
+  public handleEmojiiInput(emojiiInput : string) : void
+  {
+    // Determine if the emojii is present in the combo
+
+    // Determine damage
+
+    // Display combo status and damage
+
+    // If needed, refresh boss UI
+  }
+
+  public spawn() : void
+  {
+
+  }
+
+  public abort() : void
+  {
+    // TODO display health percentage and time
+    // TODO destroy boss
+    this.outputHandler.outputToChannel("Boss was aborted early.");
+  }
 }
--- src/inputHandler.ts
+++ src/inputHandler.ts
@@ -9,6 +9,7 @@ class InputHandler
 
   private emojiiBatleActive: boolean;
   private outputHandler : OutputHandler;
+  private boss : Boss;
   // TODO battle channel
 
   // TODO pass in channels to listen on?
@@ -18,27 +19,47 @@ class InputHandler
     this.outputHandler = outputHandler;
   };
 
+  /**
+  * Handles all potential command input strings
+  * @param {string} textInput the string to process.
+  * @param {string} username the name of the user who entered the command.
+  */
   public handleInput(textInput:string, username:string) : void
   {
-    if(textInput.startsWith(InputHandler.COMMAND_PREFIX))
+    if(textInput.startsWith(InputHandler.COMMAND_PREFIX + " "))
     {
       // Handle core commands
-      // .emojiiBattle start
-      // .emojiiBattle abort
-      // .emojiiBattle help
+      let commandStr = textInput.slice(InputHandler.COMMAND_PREFIX.length + 1); // Remove the command prefix, and the space.
+      if(commandStr == InputHandler.START_BATTLE_COMMAND)
+      {
+        this.startEmojiiBatle();
+      }
+      else if (commandStr == InputHandler.ABORT_BATTLE_COMMAND)
+      {
+        this.abortEmojiiBattle();
+      }
+      else if (commandStr == InputHandler.HELP_COMMAND)
+      {
+        this.displayHelp();
+      }
+      else
+      {
+        this.displayInvalidCommand();
+      }
     }
     else if(this.emojiiBatleActive && InputHandler.isEmojii(textInput))
     {
       // Handle boss fight commands
-      this.handleEmojiiInput(textInput);
+      this.boss.handleEmojiiInput(textInput);
     }
   }
 
-  private handleEmojiiInput(textInput : string)
-  {
-
-  }
-
+  /**
+  * Returns true if the given string is an emojii. Otherwise, returns false.
+  * @param {string} textInput the string to analyze
+  *
+  * @return {boolean} true if the string is an emojii, othewise false.
+  */
   private static isEmojii(textInput: string) : boolean
   {
     // Discord emojii rules:
@@ -55,25 +76,58 @@ class InputHandler
     return isEmojii;
   }
 
+
+  /**
+  * Starts a new emojii battle, if no other battle is active.
+  */
   private startEmojiiBatle() : void
   {
+    this.emojiiBatleActive = true;
+    // TODO start battle
 
+    // TODO spawn boss
+
+    // TODO declare battle channel
   }
 
+  /**
+  * Aborts the current emojii battle, if active
+  */
   private abortEmojiiBattle() : void
   {
-
+    this.emojiiBatleActive = false;
+    // TODO abort battle
+    this.outputHandler.outputToChannel("Battle has been aborted.");
   }
 
+  /**
+  * Displays a help message when the user types the help command
+  */
   private displayHelp() : void
   {
     if(this.emojiiBatleActive)
     {
-      "Cannot display full help options during battle.\n\nType '.emojiiBattle abort' to abort battle.";
+      this.outputHandler.outputToChannel(`Cannot display full help options during battle.\n\n
+        Type '${InputHandler.COMMAND_PREFIX} ${InputHandler.ABORT_BATTLE_COMMAND}' to abort battle.`);
     }
     else
     {
-
+      this.outputHandler.outputToChannel(`Type '${InputHandler.COMMAND_PREFIX} ${InputHandler.START_BATTLE_COMMAND}' to begin a battle.\n\n
+        During a battle, type one emojii character at a time to damage the boss.\n\n
+        To gain maximum damage output, complete an UNINTERRUPTED string of emojii to form a combo.\n\n
+        For example, if the combo is :banana: :apple: :banana:, you would type:\n\n
+        :banana:\n\n
+        :apple:\n\n
+        :banana:\n\n
+        Type '${InputHandler.COMMAND_PREFIX} ${InputHandler.ABORT_BATTLE_COMMAND}' to abort battle.`);
     }
+  }
+
+  /**
+  * Displays a help message when the user enters an invalid command.
+  */
+  private displayInvalidCommand() : void
+  {
+    this.outputHandler.outputToChannel(`Invalid command. For a list of commands, type '${InputHandler.COMMAND_PREFIX} ${InputHandler.HELP_COMMAND}'.`);
   }
 }
